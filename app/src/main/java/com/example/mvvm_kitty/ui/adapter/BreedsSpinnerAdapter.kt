4c6a058c204f439d3d6ce1eb11f40f1eb46c2185
Benjamin Metaut <metautbenjamin@hotmail.Fr>
2020-05-03 18:21:11 +0200

    designin spinner

--- app/src/main/java/com/example/mvvm_kitty/ui/adapter/BreedsSpinnerAdapter.kt
+++ app/src/main/java/com/example/mvvm_kitty/ui/adapter/BreedsSpinnerAdapter.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import com.example.mvvm_kitty.R
 import com.example.mvvm_kitty.data.local.entities.BreedEntity
-import kotlinx.android.synthetic.main.spinner_item.view.*
+import kotlinx.android.synthetic.main.custom_spinner_item.view.*
 
 
 class BreedsSpinnerAdapter(
@@ -24,19 +24,31 @@ class BreedsSpinnerAdapter(
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        return initView(position, convertView, parent)
-    }
+        var view  = convertView
+        if(convertView == null)
+        {
+            view = LayoutInflater.from(context).inflate(R.layout.custom_spinner, parent, false)
+        }
 
-    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
-        return initView(position, convertView, parent)
-    }
+        val item = getItem(position)
+        view!!.spinner_text_view.text = item.name
 
-    private fun initView(position: Int, convertView: View?, parent : ViewGroup?) : View{
-        val view = LayoutInflater.from(context).inflate(R.layout.spinner_item, parent, false)
-        view.spinner_text_view.text = getItem(position).name
         return view
     }
 
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var view  = convertView
+        if(convertView == null)
+        {
+            view = LayoutInflater.from(context).inflate(R.layout.custom_spinner_item, parent, false)
+        }
+
+        val item = getItem(position)
+        view!!.spinner_text_view.text = item.name
+
+        return view
+    }
 
     override fun getItem(position: Int): BreedEntity {
         return breedEntities[position]
